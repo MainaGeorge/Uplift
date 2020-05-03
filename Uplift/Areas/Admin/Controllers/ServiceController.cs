c333429f84d50dbda77819302dd0eeb8213e8fba
MainaGeorge <nuriszka@gmail.com>
2020-05-03 17:53:50 +0200

    Completed service controller and views

--- Uplift/Areas/Admin/Controllers/ServiceController.cs
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
 using Uplift.Models;
@@ -11,6 +13,9 @@ namespace Uplift.Areas.Admin.Controllers
     {
         private readonly IWebHostEnvironment _webHost;
         private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty]
+        public ServiceViewModel ServiceViewModel { get; set; }
 
         public ServiceController(IWebHostEnvironment webHost, IUnitOfWork unitOfWork)
         {
@@ -25,7 +30,7 @@ namespace Uplift.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Upsert(int? id)
         {
-            var serviceViewModel = new ServiceViewModel
+            ServiceViewModel = new ServiceViewModel
             {
                 Service = new Service(),
                 CategorySelectListItems = _unitOfWork.Category.GetCategoriesForDropdown(),
@@ -34,18 +39,77 @@ namespace Uplift.Areas.Admin.Controllers
 
             if (id.HasValue)
             {
-                serviceViewModel.Service = _unitOfWork.Service.Get(id.Value);
+                ServiceViewModel.Service = _unitOfWork.Service.Get(id.Value);
             }
 
-            return View(serviceViewModel);
+            return View(ServiceViewModel);
         }
 
-        // [HttpPost]
-        // [ValidateAntiForgeryToken]
-        // public IActionResult Upsert(ServiceViewModel serviceViewModel)
-        // {
-        //     if (!ModelState.IsValid) return View(serviceViewModel);
-        // }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _webHost.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (ServiceViewModel.Service.Id == 0)
+                {
+                    //New Service
+                    var fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\services");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStreams);
+                    }
+                    ServiceViewModel.Service.ImageUrl = @"\images\services\" + fileName + extension;
+
+                    _unitOfWork.Service.Add(ServiceViewModel.Service);
+                }
+                else
+                {
+                    //Edit Service
+                    var serviceFromDb = _unitOfWork.Service.Get(ServiceViewModel.Service.Id);
+                    if (files.Count > 0)
+                    {
+                        string fileName = Guid.NewGuid().ToString();
+                        var uploads = Path.Combine(webRootPath, @"images\services");
+                        var extensionNew = Path.GetExtension(files[0].FileName);
+
+                        var imagePath = Path.Combine(webRootPath, serviceFromDb.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+
+                        using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extensionNew), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStreams);
+                        }
+                        ServiceViewModel.Service.ImageUrl = @"\images\services\" + fileName + extensionNew;
+                    }
+                    else
+                    {
+                        ServiceViewModel.Service.ImageUrl = serviceFromDb.ImageUrl;
+                    }
+
+                    _unitOfWork.Service.Update(ServiceViewModel.Service);
+                }
+                _unitOfWork.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ServiceViewModel.CategorySelectListItems = _unitOfWork.Category.GetCategoriesForDropdown();
+                ServiceViewModel.FrequencySelectListItems = _unitOfWork.Frequency.GetFrequencyListForDropDown();
+                return View(ServiceViewModel);
+            }
+        }
+
+
+
 
         #region API
         [HttpGet]
