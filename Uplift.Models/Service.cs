c66106ca8015ed30da57133566296435bfdda9c2
MainaGeorge <nuriszka@gmail.com>
2020-05-03 13:07:07 +0200

    Added services model and migrations for the model.

---
+++ Uplift.Models/Service.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Uplift.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, Display(Name = "Service Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Display(Name = "Description")]
+        public string LongDescription { get; set; }
+
+        [DataType(dataType: DataType.ImageUrl)]
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int FrequencyId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [ForeignKey("FrequencyId")]
+        public Frequency Frequency { get; set; }
+
+    }
+}
