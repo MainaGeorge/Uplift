04b6fc5585801d69def82b1965a03ee477a987db
MainaGeorge <nuriszka@gmail.com>
2020-08-31 15:19:16 +0200

    Added showing the cart with items in it

--- Uplift/Areas/Customer/Controllers/CartController.cs
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.ExtensionMethods;
+using Uplift.Models;
+using Uplift.Models.ViewModels;
+using Uplift.Utility;
 
 namespace Uplift.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        [BindProperty]
+        public CartViewModel CartViewModel { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            CartViewModel = new CartViewModel
+            {
+                ServicesInCart = new List<Service>(),
+                OrderHeader = new OrderHeader()
+            };
+        }
         public IActionResult Index()
         {
-            return View();
+            var listServiceIdFromSession =
+                HttpContext.Session.RetrieveFromSession<List<int>>(AppConstants.ShoppingCart);
+
+            if (listServiceIdFromSession == null) return View(CartViewModel);
+
+            foreach (var serviceId in listServiceIdFromSession)
+            {
+                CartViewModel.ServicesInCart
+                    .Add(_unitOfWork.Service.GetFirstOrDefault(filter: s => s.Id == serviceId,
+                        includedProperties: "Frequency,Category"));
+            }
+            return View(CartViewModel);
         }
     }
 }
